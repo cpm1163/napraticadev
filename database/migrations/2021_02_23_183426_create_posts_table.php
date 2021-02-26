6ea4c79a485c4e3c9a90db4d5f320949c3ff8577
cpm1163 <carlos.mahet@gmail.com>
2021-02-26 11:48:46 -0300

    Criado migrações  modelos POST-CATEGORIA NPM PROD

---
+++ database/migrations/2021_02_23_183426_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description')->nullable();
+            $table->longText('content');
+            $table->string('slug');
+            $table->string('image_cover')->nullable();
+            $table->string('image_thumbnail')->nullable();
+            $table->dateTime('published_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
